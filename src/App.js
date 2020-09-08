22303e5ec37489c67ede629333370c364ed12c1a
櫻木真彩 <sakuragimaaya@sakuragimakotoayanoMacBook-Air.local>
2020-09-08 23:21:05 +0900

    NO.2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const [text, setText] = useState('')
-
-  const handleClick = () => {
-    setText('Hello world')
-  }
 
   return (
     <div>
-      <button onClick={handleClick}
-      >Click</button>
-      {text}
+      <button onClick={() => window.alert("Hello world")}
+      >Hello world</button>
 
     </div>
   );
