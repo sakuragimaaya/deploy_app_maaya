f24e9caee2000082acd485dd7a61faf54e72c1e8
櫻木真彩 <sakuragimaaya@sakuragimakotoayanoMacBook-Air.local>
2020-09-09 00:03:41 +0900

    NO.3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
 
+  const [count, SetCount] = useState(0);
+
+  const increment = () => {
+    SetCount(count + 1)
+    console.log(count);
+  };
+
+  const downcrement = () => {
+    SetCount(count - 1)
+    console.log(count);
+  };
+
+  const resetcrement = () => {
+    SetCount(0)
+    console.log(count);
+  };
+
+
+
   return (
     <div>
-      <button onClick={() => window.alert("Hello world")}
-      >Hello world</button>
-
+      <button onClick={increment}>いいね！</button>
+      <button onClick={downcrement}>よくないね！</button>
+      <button onClick={resetcrement}>reset</button>
+      {count}
     </div>
   );
 }
