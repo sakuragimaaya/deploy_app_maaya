7cede8872234990aa633f7b8a8cd3f7e7064721a
櫻木真彩 <sakuragimaaya@sakuragimakotoayanoMacBook-Air.local>
2020-09-19 23:04:28 +0900

    NO.6.7

--- src/App.js
+++ src/App.js
@@ -1,35 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { InputGroup, FormControl, Button } from 'react-bootstrap';
 
 function App() {
+  const [hand, setHand] = useState('');
+  const [tee, setTee] = useState("")
+  const [jajji, setJajji] = useState("")
+  const [counter, setCounter] = useState(0)
 
-  const [text, setText] = useState('');
-  const [value, setValue] = useState('');
-  const [error, setError] = useState("");
+  useEffect(() => {
+    gamehand()
+  }, [counter])
 
-  const handleClick = () => {
-    if (text === "") {
-      setError("何か文字を入力して下さい")
-
-    }
-    setValue(text)
+  const guuClick = () => {
+    setHand("ぐー")
+    cpuhand()
   }
-  console.log(text, "chek")
+
+  const chokiClick = () => {
+    setHand("ちょき")
+    cpuhand()
+
+  }
+  const paaClick = () => {
+    setHand("ぱー")
+    cpuhand()
+
+  }
+  const cpuhand = () => {
+    const num = Math.floor(Math.random() * 3)
+    console.log(num)
+    if (num === 0) {
+      setTee("ぐー")
+    } else if (num === 1) {
+      setTee("ちょき")
+    } else {
+      setTee("ぱー")
+    }
+    setCounter(prev => prev + 1)
+  }
+  const gamehand = () => {
+    if (hand === tee) {
+      setJajji("あいこです")
+    } else if (hand === 'ぐー' && tee === "ちょき") {
+      setJajji("あなたの勝ちです")
+    }
+    else if (hand === 'ぐー' && tee === "ぱー") {
+      setJajji("あなたの負けです")
+    }
+    else if (hand === 'ぱー' && tee === "ちょき") {
+      setJajji("あなたの負けです")
+    }
+    else if (hand === 'ぱー' && tee === "ぐー") {
+      setJajji("あなたの勝ちです")
+    }
+    else if (hand === 'ちょき' && tee === "ぐー") {
+      setJajji("あなたの負けです")
+    }
+    else if (hand === 'ちょき' && tee === "ぱー") {
+      setJajji("あなたの勝ちです")
+    }
+
+  }
+
 
   return (
     <div>
-      <InputGroup className="mb-3" >
-        <FormControl
-          aria-describedby="basic-addonZ"
-          defaultValue={value}
-          onChange={e => setText(e.target.value)}
-        />
-        <InputGroup.Append>
-          <Button variant="outlime-secondary" onClick={handleClick} >Button</Button>{value}
-        </InputGroup.Append>
-      </InputGroup>
-      <div color="CC0000">{error}</div>
+      <button onClick={guuClick}  >ぐー</button>
+      <button onClick={chokiClick} >ちょき</button>
+      <button onClick={paaClick} >ぱー</button>
+      <h3>あなたの手：{hand}</h3>
+      <h3>cpuの手：{tee}</h3>
+      <h3>判定中：{jajji}</h3>
+
     </div >
   )
 };
