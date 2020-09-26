6889941cc5ec70d0e232d1e021b78378e691512c
櫻木真彩 <sakuragimaaya@sakuragimakotoayanoMacBook-Air.local>
2020-09-26 20:54:59 +0900

    N0.8

--- src/App.js
+++ src/App.js
@@ -1,80 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import axios from 'axios';
 
 function App() {
-  const [hand, setHand] = useState('');
-  const [tee, setTee] = useState("")
-  const [jajji, setJajji] = useState("")
-  const [counter, setCounter] = useState(0)
+
+  const [data, setData] = useState([])
 
   useEffect(() => {
-    gamehand()
-  }, [counter])
-
-  const guuClick = () => {
-    setHand("ぐー")
-    cpuhand()
-  }
-
-  const chokiClick = () => {
-    setHand("ちょき")
-    cpuhand()
-
-  }
-  const paaClick = () => {
-    setHand("ぱー")
-    cpuhand()
-
-  }
-  const cpuhand = () => {
-    const num = Math.floor(Math.random() * 3)
-    console.log(num)
-    if (num === 0) {
-      setTee("ぐー")
-    } else if (num === 1) {
-      setTee("ちょき")
-    } else {
-      setTee("ぱー")
-    }
-    setCounter(prev => prev + 1)
-  }
-  const gamehand = () => {
-    if (hand === tee) {
-      setJajji("あいこです")
-    } else if (hand === 'ぐー' && tee === "ちょき") {
-      setJajji("あなたの勝ちです")
-    }
-    else if (hand === 'ぐー' && tee === "ぱー") {
-      setJajji("あなたの負けです")
-    }
-    else if (hand === 'ぱー' && tee === "ちょき") {
-      setJajji("あなたの負けです")
-    }
-    else if (hand === 'ぱー' && tee === "ぐー") {
-      setJajji("あなたの勝ちです")
-    }
-    else if (hand === 'ちょき' && tee === "ぐー") {
-      setJajji("あなたの負けです")
-    }
-    else if (hand === 'ちょき' && tee === "ぱー") {
-      setJajji("あなたの勝ちです")
-    }
-
-  }
-
+    axios.get('https://qiita.com/api/v2/items')
+      .then(res => setData(res.data))
+  }, [])
 
   return (
     <div>
-      <button onClick={guuClick}  >ぐー</button>
-      <button onClick={chokiClick} >ちょき</button>
-      <button onClick={paaClick} >ぱー</button>
-      <h3>あなたの手：{hand}</h3>
-      <h3>cpuの手：{tee}</h3>
-      <h3>判定中：{jajji}</h3>
-
-    </div >
+      {
+        data.map((d, index) => { return (<div key={index}>{d.title}</div>) })}
+    </div>
   )
-};
+}
+
 
 export default App;
